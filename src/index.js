0dfd7ad10191d6617dd0d1e6451383715f0af57c
Gilvan Gomes <gilvango.j@gmail.com>
2022-04-21 13:58:24 -0300

    feat: middleware insertion cors and helmets
    insertion of external middleware cors and helmets, for managing cors and headers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
 import { router } from "./routes/index.js";
 
 dotenv.config();
@@ -10,9 +12,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(morgan("dev"));
-
+app.use(helmet())
 app.use(router);
 
 app.listen(PORT, () => {
